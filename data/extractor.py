c5145dbd2cd71f28b9377f7e78415f6177fa6c1d
Pierre Marigo <=>
2019-11-24 22:32:10 +0100

    Ajout détails matchs + premiers graphes

--- data/extractor.py
+++ data/extractor.py
@@ -2,6 +2,7 @@ import json
 import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 # Affiche une donnée JSON bien formatée
 def printJSON(data):
@@ -97,11 +98,12 @@ def parseMatchesLists(matches_lists_urls):
 			if len(columns) <= 1:
 				continue
 
+			year = columns[9].text.split(' ')[2]
 			link = columns[-1].find('a')
 
 			# Ajoute une page à parser ensuite
 			if link != None:
-				matches_details_urls.append(link['href'])
+				matches_details_urls.append([year, link['href']])
 
 			# Récupère les données
 			opponent_name = columns[7].text.replace('v ', '').replace('in ', '')
@@ -116,10 +118,88 @@ def parseMatchesLists(matches_lists_urls):
 				'pointsAgainst': columns[2].text,
 				'pointsForHalfTime': columns[4].text,
 				'pointsAgainstHalfTime': columns[5].text,
-				'date': columns[9].text
+				'year': year
 			})
 
 	return [matches_list, matches_details_urls]
+
+
+# Parse une feuille de match
+# Prend en paramètre une liste au format : [[Année match, URL_details], [], ...]
+# Retourne une liste avec le nombre d'essais [0] et de pénalités [1] au total dans tous les matches
+def parseMatchDetailsList(matches_details_list_url):
+	matches_details = []
+	player_list = []
+
+	triesRegex = re.compile(r'<td.+>Tries<\/span>\s*(.+)<\/td>')
+	pensRegex = re.compile(r'<td.+>Pens<\/span>\s*(.+)<\/td>')
+
+	parenthesisRegex = re.compile(r'\([^)]+\)', re.IGNORECASE)
+
+	counts = []
+
+	for matches_details in matches_details_list_url:
+		print(matches_details[0])
+
+		html = getPageContentFromURL(matches_details[1] + '?view=right').decode('utf-8')
+
+
+		# Gestion des essais
+		triesMatches = triesRegex.findall(html)
+		countTries = 0
+
+		for m in triesMatches:
+			m = m.strip()
+			if m == 'none':
+				continue
+
+			# Format Joueur (minute, minute, minute)
+			if m.find('(') >= 0:
+				minutesMatches = parenthesisRegex.findall(m)
+				for minute in minutesMatches:
+					countTries += len(minute.split(','))
+
+			# Format Joueur [Nombre], Joueur [Nombre]
+			else:
+				for t in m.split(','):
+					try:
+						countTries += int(t[-2:].strip())
+					except ValueError:
+						countTries += 1
+
+
+		# Gestion des pénalités
+		pensMatches = pensRegex.findall(html)
+		countPens = 0
+
+		for m in pensMatches:
+			m = m.strip()
+			if m == 'none':
+				continue
+
+			# Format Joueur (minute, minute, minute)
+			if m.find('(') >= 0:
+				minutesMatches = parenthesisRegex.findall(m)
+				for minute in minutesMatches:
+					countPens += len(minute.split(','))
+
+			# Format Joueur [Nombre], Joueur [Nombre]
+			else:
+				for t in m.split(','):
+					try:
+						countPens += int(t[-2:].strip())
+					except ValueError:
+						countPens += 1
+
+		# print(countTries, countPens)
+
+		counts.append({
+			'year': matches_details[0],
+			'tries': countTries,
+			'pens': countPens
+		})
+
+	return counts
 
 
 # http://en.espn.co.uk/statsguru/rugby/team/9.html?class=1;opposition=10;template=results;type=team;view=results
@@ -137,3 +217,9 @@ writeCSVFileFromJSON(opponents_list, 'opponents_list.csv')
 [matches_list, matches_details_urls] = parseMatchesLists(matches_lists_urls)
 
 writeCSVFileFromJSON(matches_list, 'matches_list.csv')
+
+
+# Parsing de chaque match
+details_scores = parseMatchDetailsList(matches_details_urls)
+
+writeCSVFileFromJSON(details_scores, 'details_scores.csv')
---
+++ stats.r
@@ -0,0 +1,103 @@
+require(ggplot2)
+require(plyr)
+require(reshape2)
+
+########################################################
+# Pourcentage de victoire de l'équipe de France par an #
+########################################################
+
+matches = read.csv2("data/matches_list.csv", encoding="UTF-8", stringsAsFactors=FALSE, sep=',')
+
+#df=as.data.frame(aggregate(Nombre ~ Année, matches, sum))
+df=as.data.frame(matches)
+
+head(df)
+
+head(df[df$result == 'won', ])
+
+won = matches[matches$result == 'won', ]
+df_won = as.data.frame(aggregate(result ~ year, won, FUN=length), c("one", "two"))
+
+lost = matches[matches$result == 'lost', ]
+df_lost = as.data.frame(aggregate(result ~ year, lost, FUN=length))
+
+head(df_won)
+head(df_lost)
+
+# Annees sans défaite
+years_missing_lost = df_won[!df_won$year %in% df_lost$year, ]$year
+
+# Annees sans victoire
+years_missing_won = df_lost[!df_lost$year %in% df_won$year, ]$year
+
+
+# Ajout des annees sans victoire
+df_years_missing_won = data.frame(years_missing_won, rep(0, length(years_missing_won)))
+colnames(df_years_missing_won) = c('year', 'result')
+
+df_won = rbind(df_won, df_years_missing_won)
+
+# Ajout des annees sans defaite
+df_years_missing_lost = data.frame(years_missing_lost, rep(0, length(years_missing_lost)))
+colnames(df_years_missing_lost) = c('year', 'result')
+
+df_lost = rbind(df_lost, df_years_missing_lost)
+
+
+# Tri des colonnes pour la fusion
+df_won = df_won[order(df_won$year), ]
+df_lost = df_lost[order(df_lost$year), ]
+
+colnames(df_won) = c('year', 'won')
+colnames(df_lost) = c('year', 'lost')
+
+# Fusion des deux dataframes
+df_result = data.frame(df_won, df_lost$lost)
+colnames(df_result) = c('year', 'won', 'lost')
+
+
+# Calcul du pourcentage de victoire
+percent_won = df_result$won/(df_result$won + df_result$lost)
+
+df_result = cbind(df_result, percent_won)
+head(df_result, 10)
+
+
+ggplot(df_result, aes(year, y=percent_won)) +
+  geom_line()
+  #xlab("Année") +
+  #scale_x_discrete(limits=c(min(names$Année):max(names$Année))) +
+  #ylab("Longueur") +
+  #ggtitle("Longueur moyenne des noms")
+
+
+
+
+###############################################
+# Moyenne du nombre d'essais/pénalités par an #
+###############################################
+
+details = read.csv2("data/details_scores.csv", encoding="UTF-8", stringsAsFactors=FALSE, sep=',')
+
+df=as.data.frame(details)
+
+head(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
