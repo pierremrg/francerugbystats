32ce6e4a71d11054afba6f2d2dacae26db49090e
Remi Negrier <rmi.negrier@gmail.com>
2019-12-02 19:17:12 +0100

    licenciés

--- statsRemi.r
+++ statsRemi.r
@@ -1,6 +1,10 @@
 ##########################################################
 # Taille et poids moyen des joueurs de l'ASM depuis 1900 #
-##########################################################
+##############4############################################
+require(ggplot2)
+require(plyr)
+require(reshape2)
+
 
 # source donnÃ©es : http://www.cybervulcans.net/site/
 # http://www.cybervulcans.net/modules/bd/stats_taille-poids.php?poste=11&Submit=Envoyer
@@ -26,39 +30,61 @@ ggplot(df, aes(AnneeNaiss, y=mean_wgh)) +
   ylab("Poids en kg") +
   ggtitle("Evolution du poids moyen des joueurs en fonction de leur annÃ©e de naissance")
 
-
+###
 approxDataWeight = data.frame(
   with(df,
        approx(df$AnneeNaiss, df$mean_wgh, xout = seq(min(df$AnneeNaiss), max(df$AnneeNaiss), by=10), rule=1)
   )
 )
+dfInterpWeight = data.frame(a=approxDataWeight$x, w=approxDataWeight$y)
+###
+
 
 dfRealWeight = data.frame(a=df$AnneeNaiss, w=df$mean_wgh)
 
-dfInterpWeight = data.frame(a=approxDataWeight$x, w=approxDataWeight$y)
-
 ggplot() + 
   geom_line(data=dfRealWeight, aes(x=a, y=w), color='black') + 
-  geom_line(data=dfInterpWeight, aes(x=a, y=w), color='red') +
-  xlab("AnnÃ©e") +
+  geom_smooth(data=dfRealWeight, aes(x=a, y=w), color='red') + 
+  xlab("Année") +
   ylab("Poids en kg") +
-  ggtitle("Evolution du poids moyen des joueurs en fonction de leur annÃ©e de naissance")
+  ggtitle("Evolution du poids moyen des joueurs en fonction de leur année de naissance")
 
-
+###
 approxDataSize = data.frame(
   with(df,
        approx(df$AnneeNaiss, df$mean_size, xout = seq(min(df$AnneeNaiss), max(df$AnneeNaiss), by=10), rule=1)
   )
 )
 
+dfInterpSize = data.frame(a=approxDataSize$x, w=approxDataSize$y)
+###
+
 dfRealSize = data.frame(a=df$AnneeNaiss, w=df$mean_size)
 
-dfInterpSize = data.frame(a=approxDataSize$x, w=approxDataSize$y)
 
 ggplot() + 
   geom_line(data=dfRealSize, aes(x=a, y=w), color='black') + 
-  geom_line(data=dfInterpSize, aes(x=a, y=w), color='red') +
-  xlab("AnnÃ©e") +
-  ylab("Taille en cm") +
-  ggtitle("Evolution de la taille moyenne des joueurs en fonction de leur annÃ©e de naissance")
+  geom_smooth(data=dfRealSize, aes(x=a, y=w), color='red') + 
+  xlab("Année") +
+  ylab("Taille en cm en kg") +
+  ggtitle("Evolution de la taille moyenne des joueurs en fonction de leur année de naissance")
+
+
+####################################
+# Evolution du nombre de licenciés #
+##############4#####################
+
+nbPlayers = read.csv2("data/nbPlayers.csv", encoding="UTF-8", stringsAsFactors=FALSE, sep=',')
+
+head(nbPlayers)
+
+ggplot() + 
+  geom_bar(mapping = aes(x=nbPlayers$Annee, y=nbPlayers$NbLicencies), stat = "identity", color="blue", fill="blue") +
+  geom_smooth(data=nbPlayers, aes(x=Annee, y=NbLicencies), color='red', level = 0.95, span = 0.90) + 
+  xlab("Année") +
+  xlim(1960, 2020) +
+  ylab("Nombre de licenciés") +
+  ggtitle("Evolution du nombre de licenciés")
+
+
 
